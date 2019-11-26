b488a9fe478dba8da7fe81d64460d8cff1d0095b
Jopatalano <jonathan.patalano2@soprasteria.com>
2019-11-26 17:23:36 +0100

    Feat : WordHelper

--- WordLibrary/WordLibrary/ImageWord/ImageWord.cs
+++ WordLibrary/WordLibrary/ImageWord/ImageWord.cs
@@ -6,13 +6,13 @@ namespace WordLibrary.ImageWord
     public static class ImageWord
     {
 
-        public static void AddPicture(DocX document, string image)
+        public static void AddPicture(DocX document, string image, string paragraphToAdd = null)
         {
             // Ajout d'une image par un flux
             var streamImage = document.AddImage(new FileStream(image, FileMode.Open, FileAccess.Read));
             var pictureStream = streamImage.CreatePicture(150, 150);
             // TODO : Enlever le texte, pour ajout seul d'une image
-            var p3 = document.InsertParagraph("Here is the same picture added from a stream:");
+            var p3 = document.InsertParagraph(paragraphToAdd);
             p3.AppendPicture(pictureStream);
         }
     }
